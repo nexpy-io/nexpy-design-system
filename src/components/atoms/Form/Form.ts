a56cf0f570133df08420fb7e83bed4812f5fe6b9
Camila Fonseca <camilafonseca.ang7@gmail.com>
2022-09-07 22:35:11 -0300

    feat: added Form component

---
+++ src/components/atoms/Form/Form.ts
@@ -0,0 +1,9 @@
+import styled from '@xstyled/styled-components'
+
+import { System } from 'types'
+
+export type FormProps = System<'form'>
+
+const Form = styled.formBox<FormProps>``
+
+export { Form }
