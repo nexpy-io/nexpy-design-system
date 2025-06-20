4c0f45e3688dbb4f6e8e0132a39cec1cf38bf5b7
AllanOliveiraM <nallamo2016@gmail.com>
2025-06-19 21:03:06 -0300

    feat: update react props type

--- src/types/styled.ts
+++ src/types/styled.ts
@@ -1,13 +1,17 @@
+import { PropsWithRef } from 'react'
+
 import { SystemProps } from '@xstyled/styled-components'
 
 import { ElementKey, IntrinsicProps } from './internal'
 
-export type VariantSystem<V, E extends ElementKey> = {
-  variant?: V
-} & SystemProps &
-  IntrinsicProps<E>
+export type VariantSystem<V, E extends ElementKey> = PropsWithRef<
+  {
+    variant?: V
+  } & SystemProps &
+    IntrinsicProps<E>
+>
 
-export type System<E extends ElementKey> = SystemProps & IntrinsicProps<E>
+export type System<E extends ElementKey> = PropsWithRef<SystemProps & IntrinsicProps<E>>
 
 export type PropsWithAs<P> = P & {
   as?: ElementKey | undefined
